b752bb302d6431a76fff6910c2bd1d0aa6746f47
nojmana <neumannkinga7@gmail.com>
2020-06-16 23:01:45 +0200

    Read and add views

--- Project3-Sails/bookshelf/api/controllers/BookController.js
+++ Project3-Sails/bookshelf/api/controllers/BookController.js
@@ -6,7 +6,30 @@
  */
 
 module.exports = {
-  
+  list:function(req, res) {
+      Book.find({}).exec(function(err, books) {
+          if (err) {
+              res.send(500, {error: 'Database error'});
+          }
+          res.view('list', {books: books});
+      });
+  },
+
+  add:function(req, res) {
+      res.view('add');
+  },
+  create:function(req, res) {
+      var title = req.body.title;
+      var author = req.body.author;
+
+      Book.create({title:title, author: author}).exec(function(err) {
+        if (err) {
+            res.send(500, {error: 'Database error'});
+        }
+
+        res.redirect('/book/list');
+      });
+  }
 
 };
 
